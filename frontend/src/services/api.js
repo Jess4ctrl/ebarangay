101bf5ae6e0ad2f7991a02eb361b8813b20053c3
Jess4ctrl <alquezajess4@gmail.com>
2026-03-12 10:50:41 +0000

    fix api baseURL to use env variable

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.https://addressed-curtis-mechanics-recreational.trycloudflare.com + '/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
