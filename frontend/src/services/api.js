db87be2ab1f12eda87e0ad7b0a0cdd57e1d39796
Jess4ctrl <alquezajess4@gmail.com>
2026-03-12 10:59:09 +0000

    fix api baseURL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.https://addressed-curtis-mechanics-recreational.trycloudflare.com + '/api',
+  baseURL: import.meta.env.VITE_API_URL + '/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
